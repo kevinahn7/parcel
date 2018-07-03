4dcbec87805eb36e2e3f23045d0be26a14399d8a
Kevin Ahn and Renee Sarley <kevin@>
2018-07-03 10:48:43 -0700

    add initial file structure, views, model

---
+++ Parcel/Controllers/ParcelsController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Parcel.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Parcel.Controllers
+{
+    public class ParcelsController : Controller
+    {
+        [HttpGet("/parcels/form")]
+        public ActionResult Form()
+        {
+            return View();
+        }
+
+
+        [HttpGet("/parcels/cost")]
+        public ActionResult Cost()
+        {
+            // info from the form
+            int length = 2;
+            int height = 2;
+            int width = 2;
+            var weight = int.Parse(Request.Query["weight"]);
+            ShippingItem newShippingItem = new ShippingItem(length, width, height, weight);
+            int calculatedCost = newShippingItem.CalculateCost();
+            return View(calculatedCost);
+           
+        }
+    }
+}
---
+++ Parcel/Models/Parcel.cs
@@ -0,0 +1,75 @@
+﻿using System;
+namespace Parcel.Models
+{
+    public class ShippingItem
+    {
+        private int _length;
+        private int _width;
+        private int _height;
+        private int _weight;
+        private int _volume;
+        private int _costToShip;
+
+        public ShippingItem(int length, int width, int height, int weight)
+        {
+            _length = length;
+            _width = width;
+            _height = height;
+            _weight = weight;
+        }
+
+        public int GetLength()
+        {
+            return _length;
+        }
+
+        public int GetWidth()
+        {
+            return _width;
+        }
+
+        public int GetHeight()
+        {
+            return _height;
+        }
+
+        public int GetWeight()
+        {
+            return _weight;
+        }
+
+        public void SetVolume(int volume)
+        {
+            _volume = volume;
+        }
+
+        public int GetVolume()
+        {
+            return _volume;
+        }
+
+        public int GetCost()
+        {
+            return _costToShip;
+        }
+
+        public void SetCost(int cost)
+        {
+            _costToShip = cost;
+        }
+
+        public int CalculateVolume()
+        {
+            int volume = this.GetWidth() * this.GetLength() * this.GetHeight();
+            //_volume = _width * _length * _height;
+            return volume;
+        }
+
+        public int CalculateCost()
+        {
+            int costToShip = this.GetWeight();
+            return costToShip;
+        }
+
+    }
+}
